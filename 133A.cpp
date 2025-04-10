a378af02d9ca349824e85df38bf97dd570d538ac
Noora Panahi <nourap064@gmail.com>
2025-04-10 16:31:37 +0330

    HQ9+

---
+++ 133A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ; 
+
+
+int main(){
+    string p ; 
+    cin >> p ; 
+    for(char c : p){
+        if (c=='H' || c == 'Q' || c == '9'){ 
+            cout << "YES" ; 
+            return 0 ;
+        }             
+    }
+    cout << "NO" ;
+
+}
