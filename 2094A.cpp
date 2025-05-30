f92198d1a1ef3b7592a9ecd629e49475e6048ed0
Noora Panahi <nourap064@gmail.com>
2025-05-30 12:02:00 -0700

    Trippi Troppi

---
+++ 2094A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ; 
+int main(){
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(nullptr) ; 
+    int t ; 
+    cin >> t ; 
+    string s1 , s2 , s3 ;
+
+    while (t--)
+    {
+        cin >> s1 >> s2 >> s3 ;
+        cout << s1[0] << s2[0] << s3[0] << endl   ; 
+ 
+    }
+
+    
+}
